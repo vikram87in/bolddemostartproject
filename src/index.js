920d45bd897ff9b316eea0cb7fc739bda380c455
vikram87in <39241954+vikram87in@users.noreply.github.com>
2019-01-10 11:17:24 +0530

    added files

---
+++ src/index.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+export default class DemoClass {
+    constructor(props) {
+        super(props);
+        this.state = {
+            
+        };
+    }
+    
+    render() {
+       
+        return (            
+            <div>
+            </div>
+        );
+    }
+}
+DemoClass.defaultProps = {
+    
+};
+DemoClass.propTypes = {
+    
+};
